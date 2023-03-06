81fe041975d376160c2adea32b22eff0a08f9d2b
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-03-06 00:42:46 -0300

    Sequencia Fibonacci com 3 variáveis no laço For

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -9,7 +9,6 @@
             Console.Write("Digite um valor qualquer MAIOR do que 0: ");
             value = int.Parse(Console.ReadLine());
             Console.WriteLine();
-
             if (value <= 0)
             {
                 Console.WriteLine("Você digitou um valor inválido. Por favor, pressione ENTER para digitar um novo valor");
@@ -18,38 +17,17 @@
             }
         }
 
-        double[] vet = new double[value];
+        double anterior = 0;
+        double atual = 1;
+        double proximo = 1;
 
-        if (value == 1)
+        for (int i = 0; i < value; i++)
         {
-            vet[0] = 1;
-            Console.WriteLine(vet[value - 1] + ".");
+            Console.Write(atual + " ");
+            atual = proximo + anterior;
+            anterior = proximo;
+            proximo = atual;
         }
-        else if(value == 2)
-        {
-            vet[0] = 1;
-            vet[1] = 1;
-            for (int i = 0; i < (value - 1); i++)
-            {
-                Console.Write(vet[i] + ", ");
-            }
-            Console.WriteLine(vet[value - 1] + ".");
-        }
-        else
-        {
-            vet[0] = 1;
-            vet[1] = 1;
-            for (int i = 0; i < (value - 2); i++)
-            {
-                vet[i + 2] = vet[i] + vet[i + 1];
-            }
-
-            for (int i = 0; i < (value - 1); i++)
-            {
-                Console.Write(vet[i] + ", ");
-            }
-            Console.Write(vet[value - 1] + ".");
-            Console.WriteLine();
-        }
+        Console.WriteLine();
     }
 }
